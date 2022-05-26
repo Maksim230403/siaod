20e1e5ad76b609af0ca7552b0c6aa47f6173f9b9
Maksim230403 <101385571+Maksim230403@users.noreply.github.com>
2022-05-27 02:28:49 +0300

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -3,65 +3,58 @@ using namespace std;
 
 int main()
 {
-int n, m, a, b;
-
-//n-конвееры
-// m-рабочие
-
-cout«"Введите количетсво конвееров:\n";
-cin » n;
-cout«"Введите рабочих:\n"; cin »m;
-
-if (n>m)
-{
-cout«"Работы идут очень медленно,хочешь добавить рабочих?\n";
-cin»a;
-switch (a) {
-case 1:
-cout«"Введите количество новых рабочих\n";
-cin»b;
-m+=b;
-if (b=0){
-cout«"Ты ничего не изменил";
-}
-else{
-cout«"Стало больше рабочих:"«m«"\nТеперь работы ускорились";
-}
-break;
-case 2:
-cout«"Ты ничего не изменил";
-break;
-default:
-cout«"Ничего";
-break;
-}
-}
-
-if (m>n){
-cout«"Добавить еще конвееров?\n";
-cin»a;
-
-switch (a) {
-case 1:
-cout«"Введите желаемое количество конвееров\n";
-cin»b;
-n+=b;
-if (b=0){
-cout«"Ничего не изменялось";
-}
-else{
-cout«"Добавилось больше конвееров:"«n«"\nработа пошла полным ходом";
-}
-break;
-case 2:
-cout«"Ничего не изменялось";
-break;
-default:
-cout«"Ничего";
-break;
-}
-}
-else{
-cout«"Стабильность";
-}
+    int conveyor_count; int worckers_count; int choise; int delta_count;
+    cout << "Enter the number of conveys:\n";
+    cin >> conveyor_count;
+    cout << "Enter the workers:\n"; cin >> worckers_count;
+    if (conveyor_count > worckers_count)
+    {
+        cout << "The work is going very slowly, do you want to add workers?\n";
+        cin >> choise;
+        switch (choise) {
+        case 1:
+            cout << "Enter the number of new workers\n";
+            cin >> delta_count;
+            worckers_count += delta_count;
+            if (delta_count == 0) {
+                cout << "You haven't changed anything";
+            }
+            else {
+                cout << "There are more workers:" << worckers_count << "\nNow the work has accelerated";
+            }
+            break;
+        case 2:
+            cout << "You haven't changed anything";
+            break;
+        default:
+            cout << "Nothing";
+            break;
+        }
+    }
+    if (worckers_count > conveyor_count) {
+        cout << "There are a lot of workers, but who cares, do you want more conveys?\n";
+        cin >> choise;
+        switch (choise) {
+        case 1:
+            cout << "Enter the number of conveys\n";
+            cin >> delta_count;
+            conveyor_count += delta_count;
+            if (delta_count == 0) {
+                cout << "You haven't changed anything";
+            }
+            else {
+                cout << "There are more conveys:" << conveyor_count << "\nNow the work has accelerated";
+            }
+            break;
+        case 2:
+            cout << "You haven't changed anything";
+            break;
+        default:
+            cout << "Nothing";
+            break;
+        }
+    }
+    else {
+        cout << "Stability";
+    }
 }
